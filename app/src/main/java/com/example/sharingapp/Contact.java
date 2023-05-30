dc3127c0d1a013f35c42ed0f7d50e3a7ad8c8253
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-05-30 11:33:37 +0330

    Added Contact and ContactList

---
+++ app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,10 @@
+package com.example.sharingapp;
+
+public class Contact {
+    private String username;
+
+    private String email;
+
+    public String getUsername() {return this.username;}
+    public String getEmail() {return  this.email;}
+}
