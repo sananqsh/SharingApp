89341a74c24da94a1ad1a486c1562c6c22052a76
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-05-30 21:54:24 +0330

    Added Contact and ContactList

--- app/src/main/java/com/example/sharingapp/Contact.java
+++ app/src/main/java/com/example/sharingapp/Contact.java
@@ -1,10 +1,34 @@
 package com.example.sharingapp;
 
+import java.util.UUID;
+
 public class Contact {
     private String username;
-
     private String email;
+    private String id;
 
-    public String getUsername() {return this.username;}
-    public String getEmail() {return  this.email;}
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+
+        if (id == null) {
+            setId();
+        } else {
+            updateId(id);
+        }
+    }
+
+    public String getId() { return this.id; }
+
+    public void setId() { this.id = UUID.randomUUID().toString(); }
+
+    public void updateId(String id) { this.id = id; }
+
+    public String getUsername() { return this.username; }
+
+    public void setUsername(String  username) { this.username = username; }
+
+    public String getEmail() { return  this.email; }
+
+    public void setEmail(String email) { this.email = email; }
 }
