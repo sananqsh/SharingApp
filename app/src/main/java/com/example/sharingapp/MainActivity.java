3231076e0d157d54b3911e4e8a081cda42f098d3
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-05-30 10:37:44 +0330

    Added contacts menu layout resource files

--- app/src/main/java/com/example/sharingapp/MainActivity.java
+++ app/src/main/java/com/example/sharingapp/MainActivity.java
@@ -8,7 +8,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.View;
-
+import android.view.Menu;
+import android.view.MenuItem;
 /**
  * Home Activity of the App
  */
@@ -37,5 +38,23 @@ public class MainActivity extends AppCompatActivity {
     public void addItemActivity(View view) {
         Intent intent = new Intent(this, AddItemActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);//Menu Resource, Menu
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contacts:
+                Intent intent = new Intent(this, ContactsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
